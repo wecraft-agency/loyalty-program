108b6d6897620bc3d0a01958c0821ed2f30a5b7f
Marcel Bathen <moin@wecraft.agency>
2025-04-15 15:09:36 +0200

    added route to add reward product, same as lineitem/add route of shopware core

--- src/Service/Claim/ClaimController.php
+++ src/Service/Claim/ClaimController.php
@@ -4,26 +4,30 @@ namespace LoyaltyProgram\Service\Claim;
 
 use LoyaltyProgram\Service\LineItemHandler;
 
-use Shopware\Core\Checkout\Cart\LineItem\LineItem;
-use Shopware\Core\Checkout\Cart\LineItemFactoryRegistry;
-use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
-use Shopware\Core\Checkout\Customer\CustomerEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\Uuid\Uuid;
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
-use Shopware\Storefront\Event\StorefrontRedirectEvent;
-use Shopware\Storefront\Framework\Routing\StorefrontResponse;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Checkout\Cart\CartException;
+use Shopware\Core\Checkout\Cart\Error\Error;
+use Shopware\Core\Checkout\Cart\LineItemFactoryRegistry;
+use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
+use Shopware\Core\Content\Product\Exception\ProductNotFoundException;
+use Shopware\Core\Framework\Routing\RoutingException;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\Profiling\Profiler;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 use Shopware\Core\Checkout\Cart\Price\Struct\AbsolutePriceDefinition;
-use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
 use Shopware\Core\Checkout\Cart\Tax\Struct\CalculatedTaxCollection;
+use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
 use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRuleCollection;
-use Shopware\Core\Framework\Context;
 
 #[Route(defaults: ['_routeScope' => ['storefront']])]
 class ClaimController extends StorefrontController
@@ -41,59 +45,197 @@ class ClaimController extends StorefrontController
         $this->loyaltyRewardRepository = $loyaltyRewardRepository;
     }
 
-    #[Route(path: '/loyalty-claim/product', name: 'frontend.checkout.loyalty-item.add', defaults: ['_loginRequired' => true],  methods: ['POST'])]
-    public function add(Cart $cart, SalesChannelContext $context, CustomerEntity $customer): Response
+    #[Route(path: '/loyalty-claim/product', name: 'frontend.checkout.loyalty-item.add', defaults: ['_loginRequired' => true, 'XmlHttpRequest' => true],  methods: ['POST'])]
+    public function add(Cart $cart, RequestDataBag $requestDataBag, Request $request, SalesChannelContext $context, CustomerEntity $customer): Response
     {
-        if ( $_POST['type'] === LineItemHandler::TYPE ) {
-            // get reward by repository
-            $criteria = new Criteria();
-            $criteria->addFilter(new EqualsFilter('id', $_POST['id']));
+        // collect points of actual databag
+        $lineItems = $cart->getLineItems();
+        $cartSpentPoints = 0;
+        $cartAddSpentPoints = 0;
 
-            $loyaltyRewardResult = $this->loyaltyRewardRepository->search($criteria, Context::createDefaultContext());
-            $lineItemPoints = ($loyaltyRewardResult->getTotal() > 0) ? $loyaltyRewardResult->getElements()[$_POST['id']]->getPoints() : 0;
 
-            // get full cart
-            $cartPointsSpent = $lineItemPoints;
+        // current cart points
+        if ( count($lineItems->getElements()) > 0 ) {
+            foreach ( $lineItems->getElements() as $lineItem ) {
+                // Test if type is custom type, and has payload points
+                if ( $lineItem->getType() === LineItemHandler::TYPE && isset($lineItem->getPayload()['points']) ) {
+                    $quantity = $lineItem->getQuantity();
+                    $points = $lineItem->getPayload()['points'];
 
-            if ( count($cart->getLineItems()->getElements()) > 0 ) {
-                foreach ( $cart->getLineItems()->getElements() as $lineItem ) {
-                    if ( $lineItem->getType() === LineItemHandler::TYPE ) {
-                        $quantity = $lineItem->getQuantity();
-                        $points = $lineItem->getPayload()['points'];
-
-                        $cartPointsSpent = $cartPointsSpent + ($quantity * $points);
-                    }
+                    $cartSpentPoints = $cartSpentPoints + ($quantity * $points);
                 }
             }
-
-            // get customer points
-            $customerPoints = $customer->getExtensions()['loyaltyCustomer']->getPoints();
-
-            if ( $customerPoints < $cartPointsSpent ) {
-                return $this->renderStorefront('@Storefront/storefront/base.html.twig');
-            }
-
-            // Create product line item
-            $lineItem = new LineItem($_POST['id'], LineItemHandler::TYPE, null, 1);
-            $lineItem->setStackable(true);
-            $lineItem->setRemovable(true);
-            $lineItem->setLabel($_POST['name']);
-
-            // Set a zero price for the line item
-            $priceDefinition = new AbsolutePriceDefinition(0, null);
-            $calculatedTaxes = new CalculatedTaxCollection([]);
-            $taxRules = new TaxRuleCollection([]);
-            $calculatedPrice = new CalculatedPrice(0, 0, $calculatedTaxes, $taxRules);
-            $lineItem->setPrice($calculatedPrice);
-
-            // Add custom payload for points
-            $lineItem->setPayloadValue('points', $lineItemPoints); // Set the number of points
-
-            // Add the line item to the cart
-            $this->cartService->add($cart, $lineItem, $context);
         }
 
+        // new lineitems
+        $newLineItems = $requestDataBag->get('lineItems');
 
-        return $this->renderStorefront('@Storefront/storefront/base.html.twig');
+        foreach ( $newLineItems as $lineItemData ) {
+            $item = $this->factory->create($this->getLineItemArray($lineItemData, [
+                'quantity' => 1,
+                'stackable' => true,
+                'removable' => true,
+            ]), $context);
+
+            $criteria = new Criteria();
+            $criteria->addFilter(new EqualsFilter('id', $item->getId()));
+
+            // get redemption
+            $loyaltyRedemptionResult = $this->loyaltyRewardRepository->search($criteria, Context::createDefaultContext());
+
+            // get result
+            $loyaltyRedemptionItem = $loyaltyRedemptionResult->getTotal() > 0 ? $loyaltyRedemptionResult->getElements()[$item->getId()] : null;
+
+            if ( !is_null($loyaltyRedemptionItem) ) {
+                $quantity = $item->getQuantity();
+                $points = $loyaltyRedemptionItem->getPoints();
+
+                $cartAddSpentPoints = $cartAddSpentPoints + ($quantity * $points);
+            }
+        }
+
+        $cartSpentPoints = $cartSpentPoints + $cartAddSpentPoints;
+
+        return Profiler::trace('cart::add-line-item', function () use ($cart, $requestDataBag, $request, $context, $customer, $cartSpentPoints) {
+            /** @var RequestDataBag|null $lineItems */
+            $lineItems = $requestDataBag->get('lineItems');
+            if (!$lineItems) {
+                throw RoutingException::missingRequestParameter('lineItems');
+            }
+
+            $count = 0;
+
+            // Validate added rewards, by checking if points of customer are enough
+            $customerPoints = isset($customer->getExtensions()['loyaltyCustomer']) ? $customer->getExtensions()['loyaltyCustomer']->getPoints() : null;
+
+            try {
+                if ( is_null($customerPoints) || ($cartSpentPoints > $customerPoints) ) {
+                    $this->addFlash(self::DANGER, $this->trans('loyaltyProgram.global.addToCartPointsError'));
+                } else {
+                    $items = [];
+                    /** @var RequestDataBag $lineItemData */
+                    foreach ($lineItems as $lineItemData) {
+                        try {
+                            $item = $this->factory->create($this->getLineItemArray($lineItemData, [
+                                'quantity' => 1,
+                                'stackable' => true,
+                                'removable' => true,
+                            ]), $context);
+
+                            // Explicitly set stackable to true
+                            $item->setStackable(true);
+
+                            // Explicitly set removable to true
+                            $item->setRemovable(true);
+
+                            // Get redemption by id
+                            $criteria = new Criteria();
+                            $criteria->addFilter(new EqualsFilter('id', $item->getId()));
+
+                            // get redemption
+                            $loyaltyRedemptionResult = $this->loyaltyRewardRepository->search($criteria, Context::createDefaultContext());
+
+                            // get result
+                            $loyaltyRedemptionItem = $loyaltyRedemptionResult->getTotal() > 0 ? $loyaltyRedemptionResult->getElements()[$item->getId()] : null;
+
+                            // Set points for the line item
+                            $item->setPayloadValue('points', $loyaltyRedemptionItem->getPoints());
+
+                            // Set a label for the line item
+                            $item->setLabel($lineItemData->get('label', $loyaltyRedemptionItem->getName()));
+
+                            // Set a zero price for the line item
+                            $priceDefinition = new AbsolutePriceDefinition(0, null);
+                            $calculatedTaxes = new CalculatedTaxCollection([]);
+                            $taxRules = new TaxRuleCollection([]);
+                            $calculatedPrice = new CalculatedPrice(0, 0, $calculatedTaxes, $taxRules);
+                            $item->setPrice($calculatedPrice);
+
+                            $count += $item->getQuantity();
+
+                            $items[] = $item;
+                        } catch (CartException $e) {
+                            if ($e->getErrorCode() === CartException::CART_INVALID_LINE_ITEM_QUANTITY_CODE) {
+                                $this->addFlash(
+                                    self::DANGER,
+                                    $this->trans(
+                                        'error.CHECKOUT__CART_INVALID_LINE_ITEM_QUANTITY',
+                                        [
+                                            '%quantity%' => $e->getParameter('quantity'),
+                                        ]
+                                    )
+                                );
+
+                                return $this->createActionResponse($request);
+                            }
+
+                            throw $e;
+                        }
+                    }
+
+                    $cart = $this->cartService->add($cart, $items, $context);
+
+                    if (!$this->traceErrors($cart)) {
+                        $this->addFlash(self::SUCCESS, $this->trans('loyaltyProgram.global.addToCartSuccess', ['%count%' => $count]));
+                    }
+                }
+
+            } catch (ProductNotFoundException|RoutingException) {
+                $this->addFlash(self::DANGER, $this->trans('loyaltyProgram.global.addToCartError'));
+            }
+
+            return $this->createActionResponse($request);
+        });
+    }
+
+    private function traceErrors(Cart $cart): bool
+    {
+        if ($cart->getErrors()->count() <= 0) {
+            return false;
+        }
+
+        $this->addCartErrors($cart, fn (Error $error) => $error->isPersistent());
+
+        return true;
+    }
+
+    private function getLineItemArray(RequestDataBag $lineItemData, ?array $defaultValues): array
+    {
+        if ($lineItemData->has('payload')) {
+            $payload = $lineItemData->get('payload');
+
+            if (mb_strlen($payload, '8bit') > (1024 * 256)) {
+                throw RoutingException::invalidRequestParameter('payload');
+            }
+
+            $lineItemData->set('payload', json_decode($payload, true, 512, \JSON_THROW_ON_ERROR));
+        }
+
+        $lineItemArray = $lineItemData->all();
+        if ($defaultValues !== null) {
+            $lineItemArray = array_replace($defaultValues, $lineItemArray);
+        }
+
+        if (isset($lineItemArray['quantity'])) {
+            $lineItemArray['quantity'] = (int) $lineItemArray['quantity'];
+        }
+
+        if (isset($lineItemArray['stackable'])) {
+            $lineItemArray['stackable'] = (bool) $lineItemArray['stackable'];
+        }
+
+        if (isset($lineItemArray['removable'])) {
+            $lineItemArray['removable'] = (bool) $lineItemArray['removable'];
+        }
+
+        if (isset($lineItemArray['priceDefinition']['quantity'])) {
+            $lineItemArray['priceDefinition']['quantity'] = (int) $lineItemArray['priceDefinition']['quantity'];
+        }
+
+        if (isset($lineItemArray['priceDefinition']['isCalculated'])) {
+            $lineItemArray['priceDefinition']['isCalculated'] = (int) $lineItemArray['priceDefinition']['isCalculated'];
+        }
+
+        return $lineItemArray;
     }
 }
