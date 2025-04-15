90ee8953252fd6e01f26591092703bc1e269124c
Marcel Bathen <moin@wecraft.agency>
2025-04-15 18:54:21 +0200

    discount claim route base

--- src/Service/Discount/DiscountController.php
+++ src/Service/Discount/DiscountController.php
@@ -3,17 +3,29 @@
 namespace LoyaltyProgram\Service\Discount;
 
 use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(defaults: ['_routeScope' => ['storefront']])]
 class DiscountController extends StorefrontController
 {
-    #[Route(path: '/loyalty-claim/discount', name: 'frontend.discount.loyalty-reward.add', defaults: ['_loginRequired' => true, 'XmlHttpRequest' => true],  methods: ['POST'])]
+
+    private EntityRepository $loyaltyRewardRepository;
+
+    public function __construct(EntityRepository $loyaltyRewardRepository)
+    {
+        $this->loyaltyRewardRepository = $loyaltyRewardRepository;
+    }
+
+    #[Route(path: '/loyalty-claim/discount', name: 'frontend.discount.loyalty-reward.add', defaults: ['_loginRequired' => true, 'XmlHttpRequest' => true],  methods: ['POST', 'GET'])]
     public function add(Request $request, SalesChannelContext $context, CustomerEntity $customer): Response
     {
-        print_r($_POST);
 
-        return 'hi';
+        ;
     }
 }
