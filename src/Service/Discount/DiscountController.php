7025ceb83e9495272efaf24dd8c408dfb4b4f76a
Marcel Bathen <moin@wecraft.agency>
2025-04-15 18:44:01 +0200

    discount claim route base

--- src/Service/Discount/DiscountController.php
+++ src/Service/Discount/DiscountController.php
@@ -3,16 +3,17 @@
 namespace LoyaltyProgram\Service\Discount;
 
 use Shopware\Core\Checkout\Customer\CustomerEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 
 #[Route(defaults: ['_routeScope' => ['storefront']])]
 class DiscountController extends StorefrontController
 {
-    #[Route(path: '/loyalty-claim/discount', name: 'frontend.discount.loyalty-item.add', defaults: ['_loginRequired' => true, 'XmlHttpRequest' => true],  methods: ['POST'])]
-    public function addDiscountCode(Request $request, SalesChannelContext $context, CustomerEntity $customer): Response
+    #[Route(path: '/loyalty-claim/discount', name: 'frontend.discount.loyalty-reward.add', defaults: ['_loginRequired' => true, 'XmlHttpRequest' => true],  methods: ['POST'])]
+    public function add(Request $request, SalesChannelContext $context, CustomerEntity $customer): Response
     {
+        print_r($_POST);
 
+        return 'hi';
     }
 }
